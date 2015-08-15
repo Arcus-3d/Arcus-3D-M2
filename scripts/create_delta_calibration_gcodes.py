28b56d800c62f6974d03b788331c15cfc4c6efff
Daren Schwenke <darenschwenke@gmail.com>
2015-08-15 04:54:05 +0000

    New print head

--- scripts/create_delta_calibration_gcodes.py
+++ scripts/create_delta_calibration_gcodes.py
@@ -29,7 +29,7 @@ from math import sin, cos, pi
 import os
 from argparse import ArgumentParser
     
-DEF_RADIUS = 227
+DEF_RADIUS = 200
 DEF_Z_DEPTH = 20
 DEF_DIR = "../subroutines"
 DEF_G_CODE_BASE = 70
